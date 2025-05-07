96ced61f8246102cd06be99fbf72f51c234f2da6
Eric L Frederich <eric.frederich@gmail.com>
2025-05-07 14:16:26 -0400

    fix: don't add ~/.local/bin twice
    This was happening on Ubuntu because it sources ~/.profile afterwards
    and ~/.profile doesn't check that it's already there.

--- .oh-my-bash/custom/compat.sh
+++ .oh-my-bash/custom/compat.sh
@@ -1,6 +1,4 @@
 # Rocky doesn't add ~/.local/bin to PATH by default, so we need to do it manually.
-
-# Add ~/.local/bin to PATH if it exists and is not already included
-if [ -d "$HOME/.local/bin" ] && [[ ":$PATH:" != *"$HOME/.local/bin"* ]]; then
+if [ "$(bash -c 'source /etc/os-release && echo $ID')" == "rocky" ] && [[ ":$PATH:" != *"$HOME/.local/bin"* ]]; then
     PATH="$HOME/.local/bin:$PATH"
 fi
