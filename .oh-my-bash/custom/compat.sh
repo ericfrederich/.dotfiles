351a46c1317423eddec7d850a6aa21c1848659c1
Eric L Frederich <eric.frederich@gmail.com>
2025-08-21 14:15:11 -0400

    feat: fedora support

--- .oh-my-bash/custom/compat.sh
+++ .oh-my-bash/custom/compat.sh
@@ -1,4 +1,4 @@
-# Rocky doesn't add ~/.local/bin to PATH by default, so we need to do it manually.
-if [ "$(bash -c 'source /etc/os-release && echo $ID')" == "rocky" ] && [[ ":$PATH:" != *"$HOME/.local/bin"* ]]; then
+# Rocky and Fedora don't add ~/.local/bin to PATH by default, so we need to do it manually.
+if bash -c 'source /etc/os-release && echo $ID' | grep -Eq '^(rocky|fedora)$' && [[ ":$PATH:" != *"$HOME/.local/bin"* ]]; then
     PATH="$HOME/.local/bin:$PATH"
 fi
