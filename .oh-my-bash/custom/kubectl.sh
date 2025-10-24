d25d460c4a8fd32627f2f5919ce2bae0906764bd
Eric L Frederich <eric.frederich@gmail.com>
2025-10-24 11:35:38 -0400

    run pre-commit

--- .oh-my-bash/custom/kubectl.sh
+++ .oh-my-bash/custom/kubectl.sh
@@ -1,5 +1,5 @@
 if command -v kubectl >/dev/null 2>&1; then
-	. <(kubectl completion bash)
+    . <(kubectl completion bash)
 elif [ -x "$HOME/.local/bin/kubectl" ]; then
     . <($HOME/.local/bin/kubectl completion bash)
 fi
